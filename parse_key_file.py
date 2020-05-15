d1f68278aefb8588a5dde72acc4815e71a2ae39c
RGBmarya <msarya@icloud.com>
2020-05-15 16:54:38 -0700

    added initial code to extract_auth_keys

--- parse_key_file.py
+++ parse_key_file.py
@@ -4,4 +4,8 @@ from typing import Tuple
 
 def extract_auth_keys(path: str) -> Tuple(str,str):
     """Retrieve developer keys from the specified file"""
-    pass
+    with open (path) as file:
+        text=file.readlines()        
+        app_id=text[0].split(":")[1]
+        app_key=text[1].split(":")[1]    
+        return(app_id,app_key)
