d7eec2332ec75fb5bc3e754378a124e01e7d7ae3
c4llmeco4ch <cbb692@gmail.com>
2020-05-15 10:36:41 -0700

    Added initial function headers for building API query

---
+++ parse_key_file.py
@@ -0,0 +1,7 @@
+import typing
+from typing import Tuple
+
+
+def extract_auth_keys(path: str) -> Tuple(str,str):
+    """Retrieve developer keys from the specified file"""
+    pass
---
+++ query_builder.py
@@ -0,0 +1,7 @@
+import requests
+import typing
+from typing import Dict
+
+def build_query(*args) -> Dict[str, str]:
+    """Creates the dictionary of parameters needed to query the Edamam API"""
+    pass
