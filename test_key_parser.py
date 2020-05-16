7e7410bb1fc5d28b817c4b9aa59fcf654f973d07
RGBmarya <msarya@icloud.com>
2020-05-15 17:26:24 -0700

    Added initial tests for extracting keys

---
+++ test_key_parser.py
@@ -0,0 +1,8 @@
+import pytest
+from . import parse_key_file
+
+class TestExtractKeys:
+
+    def test_normal_file(self):
+        assert parse_key_file.extract_auth_keys("normal1.txt")==("thisisnormal1id1234", "thisisnormal1key1234")
+        assert parse_key_file.extract_auth_keys("normal2.txt")==("cat", "dog")
