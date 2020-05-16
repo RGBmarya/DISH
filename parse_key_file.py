6287036e43388291c231f1c6f33bcb1fe44fc255
RGBmarya <msarya@icloud.com>
2020-05-15 17:28:33 -0700

    Fixed type hint and stripped key/id whitespace

--- parse_key_file.py
+++ parse_key_file.py
@@ -2,10 +2,10 @@ import typing
 from typing import Tuple
 
 
-def extract_auth_keys(path: str) -> Tuple(str,str):
+def extract_auth_keys(path: str) -> Tuple[str,str]:
     """Retrieve developer keys from the specified file"""
     with open (path) as file:
         text=file.readlines()        
-        app_id=text[0].split(":")[1]
-        app_key=text[1].split(":")[1]    
+        app_id=text[0].split(":")[1].strip()
+        app_key=text[1].split(":")[1].strip()
         return(app_id,app_key)
