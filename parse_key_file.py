d57d161fce57fbbeec4221a4b40d9e05502ff715
c4llmeco4ch <cbb692@gmail.com>
2020-05-22 10:27:57 -0700

    Added initial headers for request functions

--- parse_key_file.py
+++ parse_key_file.py
@@ -1,11 +1,17 @@
 import typing
+from requests import Session
 from typing import Tuple
 
 
 def extract_auth_keys(path: str) -> Tuple[str,str]:
-    """Retrieve developer keys from the specified file"""
+    """Retrieves developer keys from the specified file"""
     with open (path) as file:
         text=file.readlines()        
         app_id=text[0].split(":")[1].strip()
         app_key=text[1].split(":")[1].strip()
         return(app_id,app_key)
+
+
+def create_session(keys: Tuple[str, str]) -> Session:
+    """Creates the session with which API information will be requested"""
+    pass
--- query_builder.py
+++ query_builder.py
@@ -1,7 +1,12 @@
 import requests
 import typing
-from typing import Dict
+from typing import Dict, List
 
 def build_query(*args) -> Dict[str, str]:
     """Creates the dictionary of parameters needed to query the Edamam API"""
+    pass
+
+
+def send_query(ingredients: List[str], *args) -> str:
+    """Sends an API request to the Edamam API"""
     pass
