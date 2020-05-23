ae7de4a80a54ba16aa62f34239768d63948755f4
RGBmarya <msarya@icloud.com>
2020-05-22 17:33:57 -0700

    Began adding dictionary to build query

--- query_builder.py
+++ query_builder.py
@@ -2,9 +2,19 @@ import requests
 import typing
 from typing import Dict, List
 
-def build_query(*args) -> Dict[str, str]:
+def build_query(**kwargs) -> Dict[str, str]:
     """Creates the dictionary of parameters needed to query the Edamam API"""
-    pass
+    params={}
+    for name, value in kwargs.items():
+        if name=="from" and type(value) is int and value >= 0:
+            params[name]=value
+        elif name=="to" and type(value) is int and value >= 0:
+            params[name]=value
+        elif name=="ingr":
+            pass
+        elif name=="diet":
+            pass
+    return params
 
 
 def send_query(ingredients: List[str], *args) -> str:
