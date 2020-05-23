2201f3c0a62d1f0da705fb45c77d76e5dff037f8
RGBmarya <msarya@icloud.com>
2020-05-22 17:35:04 -0700

    Began adding edge cases to key extraction

--- test_key_parser.py
+++ test_key_parser.py
@@ -6,3 +6,5 @@ class TestExtractKeys:
     def test_normal_file(self):
         assert parse_key_file.extract_auth_keys("normal1.txt")==("thisisnormal1id1234", "thisisnormal1key1234")
         assert parse_key_file.extract_auth_keys("normal2.txt")==("cat", "dog")
+        assert parse_key_file.extract_auth_keys("oneline.txt")==("nothing", "")
+        # file only one line, file with no id/key only title
