903a239cafa3ea0b6936549c29f583ccf9a82242
RGBmarya <msarya@icloud.com>
2020-05-29 15:26:47 -0700

    Added elif statements for additional parameters in build_query() function. Left excluded and callback parameters with pass.

--- query_builder.py
+++ query_builder.py
@@ -2,17 +2,36 @@ import requests
 import typing
 from typing import Dict, List
 
+
 def build_query(**kwargs) -> Dict[str, str]:
     """Creates the dictionary of parameters needed to query the Edamam API"""
-    params={}
+    params = {}
     for name, value in kwargs.items():
-        if name=="from" and type(value) is int and value >= 0:
-            params[name]=value
-        elif name=="to" and type(value) is int and value >= 0:
-            params[name]=value
-        elif name=="ingr":
+        if name == "from" and type(value) is int and value >= 0:
+            params[name] = value
+        elif name == "to" and type(value) is int and value >= 0:
+            params[name] = value
+        elif name == "ingr" and type(value) is int and value >= 0:
+            params[name] = value
+        elif name == "diet" and type(value) is str and value >= 0:
+            params[name] = value
+        elif name == "health" and type(value) is str and value >= 0:
+            params[name] = value
+        elif name == "cuisineType" and type(value) is str and value >= 0:
+            params[name] = value
+        elif name == "mealType" and type(value) is str and value >= 0:
+            params[name] = value
+        elif name == "dishType" and type(value) is str and value >= 0:
+            params[name] = value
+        elif name == "calories" and type(value) is range(minCal, maxCal) and value >= 0:
+            params[name] = value
+    #create minCal and maxCal variables if code is correct
+        elif name == "time" and type(value) is range(minTime, maxTime) and value >= 0:
+            params[name] = value
+    #create minTime and maxTime variables if code is correct
+        elif name == "excluded" and type(value) is str and value >= 0:
             pass
-        elif name=="diet":
+        elif name == "callback" and type(value) is str and value >=0:
             pass
     return params
 
