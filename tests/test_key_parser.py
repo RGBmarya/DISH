550eb4e7dc80f467f198ae9207ab064aac8b5f00
c4llmeco4ch <cbb692@gmail.com>
2020-05-29 10:45:04 -0700

    Restructured tests into new directory

---
+++ tests/test_key_parser.py
@@ -0,0 +1,10 @@
+import pytest
+from DISH import parse_key_file
+
+class TestExtractKeys:
+
+    def test_normal_file(self):
+        assert parse_key_file.extract_auth_keys("normal1.txt")==("thisisnormal1id1234", "thisisnormal1key1234")
+        assert parse_key_file.extract_auth_keys("normal2.txt")==("cat", "dog")
+        assert parse_key_file.extract_auth_keys("oneline.txt")==("nothing", "")
+        # file only one line, file with no id/key only title
