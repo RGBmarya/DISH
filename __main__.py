754eefbbf89ce9b1a3fe84727bf67c75b731f6ae
c4llmeco4ch <cbb692@gmail.com>
2020-06-05 10:36:01 -0700

    Added main function for pulling things together

---
+++ __main__.py
@@ -0,0 +1,20 @@
+import sys, getopt
+from typing import List
+
+SHORT_ARGS = 'i:f:t:d:h:'
+LONG_ARGS = ['help', 'ingredients=', 'from=',
+             'to=', 'diet=', 'health=', 'cuisine-type=',
+             'meal-type=', 'dish-type=']
+
+
+def go(args: List[str]):
+    """The main runner of the DISH program
+
+    Args:
+        args (List[str]): the list of arguments 
+                            passed from the command line
+    """
+    pass
+
+if __name__ == '__main__':
+    go(sys.argv[1:])
