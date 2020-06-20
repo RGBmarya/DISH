3ca951688195034854a10fda5aab63ac2aab48c4
RGBmarya <msarya@icloud.com>
2020-06-19 19:08:25 -0700

    Completed commands and help messages for command line

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
-import sys, getopt
+import sys
+import getopt
+import click
 import modules.parse_json as pj
 import modules.parse_key_file as pk
 import modules.send_request as sr
@@ -7,12 +9,16 @@ from typing import List
 
 API_URL = "https://api.edamam.com/search"
 
-SHORT_ARGS = 'i:f:t:d:h:'
-LONG_ARGS = ['help', 'ingredients=', 'from=',
-             'to=', 'diet=', 'health=', 'cuisine-type=',
-             'meal-type=', 'dish-type=']
 
-
+@click.command()
+@click.option('--ingredients', '-i', help='Ingredients you have available to create your dish', required = True)
+@click.option('--from', 'f', help='Begin listing recipes at this index', default=-1)
+@click.option('--to', 't', help='Maximum number of recipes to display', default=-1)
+@click.option('--diet', 'd', help='Diet label: choose from “balanced”, “high-protein”, “high-fiber”, “low-fat”, “low-carb”, “low-sodium”')
+@click.option('--health', '-h', help='Health label: choose from “vegan”, “vegetarian”, “paleo”, “dairy-free”, “gluten-free”, “wheat-free”, “fat-free”, “low-sugar”, “egg-free”, “peanut-free”, “tree-nut-free”, “soy-free”, “fish-free”, “shellfish-free”')
+@click.option('--cuisine-type', '-c', help='Cuisine Type: choose from “American”, “Asian”, “British”, “Caribbean”, “Central Europe”, “Chinese”, “Eastern Europe”, “French”, “Indian”, “Japanese”, “Kosher”, “Mediterranean”, “Mexican”, “Middle Eastern”, “Nordic”, “South American”, “South East Asian”')
+@click.option('--meal-type', '-m', help='The type of meal a recipe belongs to: choose from “breakfast”, “lunch”, “dinner”, snack”, “teatime”')
+@click.option('--dish-type', '-d', help='The dish type a recipe belongs to: choose from “biscuits and cookies”, “bread”, “cereals”, “condiments and sauces”, “drinks”, “desserts”, “main course”, “pancake”, “preps”, “preserve”, “salad”, “sandwiches”, “side dish”, “soup”, “starter”, “sweets”,')
 def go(args: List[str]):
     """The main runner of the DISH program
 
@@ -20,15 +26,18 @@ def go(args: List[str]):
         args (List[str]): the list of arguments 
                             passed from the command line
     """
+
     id, key = pk.extract_auth_keys("key.txt")
-    params = qb.build_query_params(to = 3, health = "alcohol-free", calories = "591-722")
+    params = qb.build_query_params(
+        to=3, health="alcohol-free", calories="591-722")
     params["q"] = "chicken"
     params["app_id"] = id
     params["app_key"] = key
-    r = sr.send_request(API_URL, params = params)
+    r = sr.send_request(API_URL, params=params)
     cb = pj.extract_recipes(r)
     for dish in cb:
-        pj.display_recipes(dish)    
+        pj.display_recipes(dish)
+
 
 if __name__ == '__main__':
     go(sys.argv[1:])
