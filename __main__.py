5dc22a87fcaf7b3895c1958f31809be8e9c32755
RGBmarya <msarya@icloud.com>
2020-06-21 12:41:54 -0700

    Limited diet and health parameters and removed typing.List (unused)

--- __main__.py
+++ __main__.py
@@ -1,43 +1,56 @@
 import sys
-import getopt
 import click
 import modules.parse_json as pj
 import modules.parse_key_file as pk
 import modules.send_request as sr
 import modules.query_builder as qb
-from typing import List
 
 API_URL = "https://api.edamam.com/search"
 
 
 @click.command()
-@click.option('--ingredients', '-i', help='Ingredients you have available to create your dish', required = True)
-@click.option('--from', 'f', help='Begin listing recipes at this index', default=-1)
-@click.option('--to', 't', help='Maximum number of recipes to display', default=-1)
-@click.option('--diet', 'd', help='Diet label: choose from “balanced”, “high-protein”, “high-fiber”, “low-fat”, “low-carb”, “low-sodium”')
-@click.option('--health', '-h', help='Health label: choose from “vegan”, “vegetarian”, “paleo”, “dairy-free”, “gluten-free”, “wheat-free”, “fat-free”, “low-sugar”, “egg-free”, “peanut-free”, “tree-nut-free”, “soy-free”, “fish-free”, “shellfish-free”')
-@click.option('--cuisine-type', '-c', help='Cuisine Type: choose from “American”, “Asian”, “British”, “Caribbean”, “Central Europe”, “Chinese”, “Eastern Europe”, “French”, “Indian”, “Japanese”, “Kosher”, “Mediterranean”, “Mexican”, “Middle Eastern”, “Nordic”, “South American”, “South East Asian”')
-@click.option('--meal-type', '-m', help='The type of meal a recipe belongs to: choose from “breakfast”, “lunch”, “dinner”, snack”, “teatime”')
-@click.option('--dish-type', '-d', help='The dish type a recipe belongs to: choose from “biscuits and cookies”, “bread”, “cereals”, “condiments and sauces”, “drinks”, “desserts”, “main course”, “pancake”, “preps”, “preserve”, “salad”, “sandwiches”, “side dish”, “soup”, “starter”, “sweets”,')
-def go(args: List[str]):
+@click.option('--ingredients', '-i',
+              help='Ingredients you have available to create your dish',
+              required=True)
+@click.option('-f', help='Begin listing recipes at this index', default=-1)
+@click.option('--to', '-t',
+              help='Maximum number of recipes to display', default=-1)
+@click.option('--diet', '-d', help='Diet label: choose from “balanced”, \
+             “high-protein”, “low-fat”, “low-carb”')
+@click.option('--health', help='Health label: choose from “vegan”, \
+             “vegetarian”, “sugar-conscious”, “peanut-free”, \
+             “tree-nut-free”, “fat-free”, “alcohol-free”')
+@click.option('--meal-type', '-m', help='The type of meal a recipe belongs \
+              to: choose from “breakfast”, “lunch”, “dinner”, “snack”, \
+              “teatime”')
+def go(ingredients, f, to, diet, health, meal_type):
     """The main runner of the DISH program
 
     Args:
-        args (List[str]): the list of arguments 
+        args (List[str]): the list of arguments
                             passed from the command line
     """
 
     id, key = pk.extract_auth_keys("key.txt")
     params = qb.build_query_params(
-        to=3, health="alcohol-free", calories="591-722")
-    params["q"] = "chicken"
+        f=f,
+        to=to,
+        diet=diet,
+        health=health,
+        mealType=meal_type
+    )
+    params["q"] = ingredients
     params["app_id"] = id
     params["app_key"] = key
     r = sr.send_request(API_URL, params=params)
+    if r.status_code != 200:
+        print("invalid request")
+        print(r.content)
+        sys.exit()
     cb = pj.extract_recipes(r)
     for dish in cb:
         pj.display_recipes(dish)
 
 
 if __name__ == '__main__':
-    go(sys.argv[1:])
+    go()
